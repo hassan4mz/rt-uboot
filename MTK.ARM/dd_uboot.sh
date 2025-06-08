d333f7417b4c17d5afd8a75f1623c34fed054210
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2025-06-08 22:17:08 +0300

    Delete MTK.ARM directory

--- MTK.ARM/dd_uboot.sh
+++
@@ -1,7 +0,0 @@
-UBOOT=u-boot.bin
-DEVICE=/dev/sdg
-
-sudo dd if=${UBOOT}  of=${DEVICE}  bs=1024 seek=320
-sync
-
-eject ${DEVICE}
--- MTK.ARM/lib_rtos/libcpu_armv8/armv8.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ARMV8_H__
-#define __ARMV8_H__
-
-
-
-#define __REG32(x)  (*((volatile unsigned int *)(x)))
-#define __REG16(x)  (*((volatile unsigned short *)(x)))
-
-#endif
